a37de8290a58c0241f743c4211441d6aa9ea4851
SaadArafat <saadarafat_it@yahoo.com>
2025-05-25 02:21:32 +0300

    Update MovieDetails.js

--- starter/frontend/src/components/MovieDetails.js
+++ starter/frontend/src/components/MovieDetails.js
@@ -1,20 +1,29 @@
-import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 
-function MovieDetail({ movie }) {
-  const [details, setDetails] = useState(null);
-  useEffect(() => {
-    axios.get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`).then((response) => {
-      setDetails(response.data);
-    });
+import React from "react";
+import axios from "axios";
+
+function MovieDetails({ movie }) {
+  const [details, setDetails] = React.useState(null);
+
+  React.useEffect(() => {
+    axios
+      .get(`${process.env.REACT_APP_MOVIE_API_URL}/movies/${movie.id}`)
+      .then((response) => {
+        setDetails(response.data);
+      })
+      .catch((error) => {
+        console.error("Error fetching movie details:", error);
+      });
   }, [movie]);
+
+  if (!details) return <p>Loading movie details...</p>;
 
   return (
     <div>
-      <h2>{details?.movie.title}</h2>
-      <p>{details?.movie.description}</p>
+      <h2>{details.title}</h2>
+      <p>{details.description}</p>
     </div>
   );
 }
 
-export default MovieDetail;
+export default MovieDetails;
