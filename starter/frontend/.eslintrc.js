915dd6ea6de3ab91c0a7bd76754c303aa9400527
SaadArafat <saadarafat_it@yahoo.com>
2025-05-25 02:11:42 +0300

    Update .eslintrc.js

--- starter/frontend/.eslintrc.js
+++ starter/frontend/.eslintrc.js
@@ -2,16 +2,16 @@ module.exports = {
   env: {
     browser: true,
     es2021: true,
-    jest: true
+    jest: true,
   },
   extends: ["react-app", "eslint:recommended", "plugin:prettier/recommended"],
   overrides: [],
   parserOptions: {
     ecmaVersion: "latest",
-    sourceType: "module"
+    sourceType: "module",
   },
   plugins: ["prettier"],
   rules: {
-    "prettier/prettier": "error"
-  }
+    "prettier/prettier": "error",
+  },
 };
